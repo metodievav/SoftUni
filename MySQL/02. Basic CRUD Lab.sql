c747ecbea1f22843f5ed63bbe1a33cde1bd26a9a
metodievav <metodievav@abv.bg>
2023-05-16 11:49:15 +0300

    update

---
+++ MySQL/02. Basic CRUD Lab.sql
@@ -0,0 +1,57 @@
+USE hotel;
+
+-- 1
+SELECT 
+    id, first_name, last_name, job_title
+FROM
+    employees
+ORDER BY id;
+
+-- 2
+SELECT 
+    id,
+    CONCAT(first_name, ' ', last_name) AS 'full_name', job_title, salary
+FROM
+    employees
+WHERE
+    salary > 1000
+ORDER BY id;
+
+-- 3
+UPDATE employees 
+SET 
+    salary = salary + 100
+WHERE
+    job_title = 'Manager';
+
+SELECT 
+    salary
+FROM
+    employees;
+    
+-- 4
+SELECT 
+    *
+FROM
+    employees
+ORDER BY salary DESC
+LIMIT 1;
+
+-- 5
+SELECT 
+    *
+FROM
+    employees
+WHERE department_id = 4 AND salary >= 1000
+ORDER BY id;
+
+-- 6
+DELETE FROM employees 
+WHERE
+    department_id = 1 OR department_id = 2;
+
+SELECT 
+    *
+FROM
+    employees
+ORDER BY id;
