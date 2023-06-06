61a109f11b737be11dd4b93582f37f8a31f2cc9c
metodievav <metodievav@abv.bg>
2023-06-06 19:41:55 +0300

    update

--- MySQL/06. Subqueries and JOINs/06. Subqueries and JOINs Exercise.sql
+++ MySQL/06. Subqueries and JOINs/06. Subqueries and JOINs Exercise.sql
@@ -79,3 +79,159 @@ WHERE
 ORDER BY e.hire_date;
 
 # 7
+SELECT 
+    e.employee_id, e.first_name, p.name AS 'project_name'
+FROM
+    employees AS e
+        JOIN
+    employees_projects AS ep ON ep.employee_id = e.employee_id
+        JOIN
+    projects AS p ON p.project_id = ep.project_id
+WHERE
+    DATE(p.start_date) > '2002-08-13'
+        AND p.end_date IS NULL
+ORDER BY e.first_name , p.name
+LIMIT 5;
+
+# 8
+SELECT 
+    e.employee_id,
+    e.first_name,
+    IF(YEAR(p.start_date) >= 2005,
+        NULL,
+        p.name) AS 'project_name'
+FROM
+    employees AS e
+        JOIN
+    employees_projects AS ep ON e.employee_id = ep.employee_id
+        JOIN
+    projects AS p ON ep.project_id = p.project_id
+WHERE
+    e.employee_id = 24
+ORDER BY p.name ASC;
+
+# 9
+SELECT 
+    e.employee_id,
+    e.first_name,
+    e.manager_id,
+    m.first_name AS 'manager_name'
+FROM
+    employees AS e
+       JOIN
+    employees AS m ON m.employee_id = e.manager_id
+WHERE
+    e.manager_id IN (3 , 7)
+ORDER BY e.first_name;
+
+# 10
+SELECT 
+    e.employee_id,
+    CONCAT_WS(' ', e.first_name, e.last_name) AS 'employee_name',
+    CONCAT_WS(' ', m.first_name, m.last_name) AS 'manager_name',
+    d.name AS 'department_name'
+FROM
+    employees AS e
+        JOIN
+    employees AS m ON e.manager_id = m.employee_id
+        JOIN
+    departments AS d ON e.department_id = d.department_id
+WHERE
+    e.manager_id IS NOT NULL
+ORDER BY e.employee_id
+LIMIT 5;
+
+# 11
+SELECT 
+    AVG(salary) AS 'min_average_salary'
+FROM
+    employees
+GROUP BY department_id
+ORDER BY min_average_salary ASC
+LIMIT 1;
+
+# 12
+USE geography;
+
+SELECT 
+    mc.country_code, m.mountain_range, p.peak_name, p.elevation
+FROM
+    peaks AS p
+        JOIN
+    mountains AS m ON p.mountain_id = m.id
+        JOIN
+    mountains_countries AS mc ON m.id = mc.mountain_id
+WHERE
+    mc.country_code = 'BG'
+        AND p.elevation > 2835
+ORDER BY p.elevation DESC;
+
+# 13
+SELECT 
+    mc.country_code, COUNT(m.mountain_range) AS 'mountain_range'
+FROM
+    mountains_countries AS mc
+        JOIN
+    mountains AS m ON mc.mountain_id = m.id
+WHERE
+    mc.country_code IN ('BG' , 'RU', 'US')
+GROUP BY mc.country_code
+ORDER BY mountain_range DESC;
+
+# 14
+SELECT 
+    c.country_name, r.river_name
+FROM
+    rivers AS r
+        JOIN
+    countries_rivers AS cr ON r.id = cr.river_id
+        RIGHT JOIN
+    countries AS c ON cr.country_code = c.country_code
+WHERE
+    c.continent_code = 'AF'
+ORDER BY c.country_name
+LIMIT 5;
+
+# 15
+SELECT 
+    c.continent_code,
+    c.currency_code,
+    COUNT(*) AS 'currency_usage'
+FROM
+    countries AS c
+GROUP BY c.continent_code , c.currency_code
+HAVING currency_usage > 1
+    AND currency_usage = (SELECT 
+        COUNT(*) AS count_of_currencies
+    FROM
+        countries AS c2
+    WHERE
+        c2.continent_code = c.continent_code
+    GROUP BY c2.currency_code
+    ORDER BY count_of_currencies DESC
+    LIMIT 1)
+ORDER BY c.continent_code , c.currency_code;
+
+# 16
+SELECT 
+    COUNT(*) AS 'country_count'
+FROM
+    countries AS c
+        LEFT JOIN
+    mountains_countries AS mc ON c.country_code = mc.country_code
+WHERE
+    mc.mountain_id IS NULL;
+    
+# 17
+SELECT 
+    c.country_name,
+    MAX(p.elevation) AS 'highest_peak_elevation',
+    MAX(r.length) AS 'longest_river_length'
+FROM countries AS c
+        LEFT JOIN mountains_countries AS mc ON c.country_code = mc.country_code
+        LEFT JOIN peaks AS p ON mc.mountain_id = p.mountain_id
+        LEFT JOIN countries_rivers AS cr ON c.country_code = cr.country_code
+        LEFT JOIN rivers AS r ON cr.river_id = r.id
+GROUP BY c.country_name
+ORDER BY highest_peak_elevation DESC , longest_river_length DESC , c.country_name
+LIMIT 5;
